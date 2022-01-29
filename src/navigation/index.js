36b72109543b88cc93c108fd37a28fc03141b0a2
Mariano Vergara <marianovf19@outlook.com>
2022-01-29 12:47:17 -0300

    color y diseño para el header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,5 +1,7 @@
+import { COLORS } from '../utils/constants/colors';
 import Categories from '../screens/categories/index';
 import { NavigationContainer } from '@react-navigation/native';
+import { Platform } from 'react-native';
 import ProductDetail from '../screens/product-detail/index';
 import Products from '../screens/products/index';
 import React from 'react';
@@ -10,15 +12,17 @@ const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
     return (
         <NavigationContainer >
-            <Stack.Navigator initialRouteName='Categories' >
-                <Stack.Screen name="Categories" component={Categories} options={{title: 'Home',
-            headerStyle: {
-                backgroundColor: '#f4511e',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },}} />
+            <Stack.Navigator initialRouteName='Categories'
+             screenOptions={{
+                headerStyle: {
+                    backgroundColor: Platform.OS === 'android' ? COLORS.primary : '',
+                },
+                headerTintColor: Platform.OS === 'android' ? 'white' : COLORS.primary,
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }} >
+                <Stack.Screen name="Categories" component={Categories} />
                 <Stack.Screen name="Products" component={Products} />
                 <Stack.Screen name="ProductDetail" component={ProductDetail} />
             </Stack.Navigator>
