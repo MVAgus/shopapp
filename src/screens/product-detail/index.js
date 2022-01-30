70274f4b8e09ca2780439e9ffb4918e559a678d0
Mariano Vergara <marianovf19@outlook.com>
2022-01-30 20:18:35 -0300

    se añadieron a la vista de detalles los atributos del bread

--- src/screens/product-detail/index.js
+++ src/screens/product-detail/index.js
@@ -1,5 +1,4 @@
 import {
-   Button,
    SafeAreaView,
    Text,
    View
@@ -7,23 +6,23 @@ import {
 
 import React from 'react';
 import styles from './styles';
-import { useSelector } from 'react-redux';
 
 const ProductDetail = ({navigation, route}) => {
-   const bread = useSelector(state => state.breads.selected);
+   
+  const {product} = route.params
+  
    return (
      <SafeAreaView style={styles.container}>
        <View style={styles.container}>
-         <Text>{bread.name}</Text>
-         <Text>{bread.description}</Text>
-         <Text>{bread.weight}</Text>
-         <Text>$ {bread.price}</Text>
+         <Text>{product.name}</Text>
+         <Text>{product.description}</Text>
+         <Text>{product.weight}</Text>
+         <Text>$ {product.price}</Text>
        </View>
      </SafeAreaView>
    );
  };
  
- 
- 
+
  export default ProductDetail;
  
