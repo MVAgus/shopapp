09e5b2e7bb30e816ebcfc88a2dc8ec29020ccba6
Mariano Vergara <marianovf19@outlook.com>
2022-01-31 18:41:40 -0300

    se separo la navigation en shop y cart

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,47 +1,15 @@
-import { COLORS } from '../utils/constants/colors';
-import Categories from '../screens/categories/index';
+import CartStackNavigation from './cart';
 import { NavigationContainer } from '@react-navigation/native';
-import { Platform } from 'react-native';
-import ProductDetail from '../screens/product-detail/index';
-import Products from '../screens/products/index';
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ShopStackNavigation from './shop';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 
 const AppNavigation = () => {
     return (
         <NavigationContainer >
-            <Stack.Navigator initialRouteName='Categories'
-             screenOptions={{
-                headerStyle: {
-                    backgroundColor: Platform.OS === 'android' ? COLORS.primary : '',
-                },
-                headerTintColor: Platform.OS === 'android' ? 'white' : COLORS.primary,
-                headerTitleStyle: {
-                    fontWeight: 'bold'
-                }
-            }} >
-                <Stack.Screen name="Categories" component={Categories} />
-                {/*en el caso de que querramos que la vista tenga un nombre en particular
-                vamos a utilizar options = {{title: 'titulo}}*/}
-                <Stack.Screen 
-                name="Products" 
-                component={Products}
-                options={({route}) => ({
-                    title:route.params.name,
-                    headerStyle: {
-                        backgroundColor: route.params.color
-                    }})}
-                
-                />
-                <Stack.Screen 
-                name="ProductDetail" 
-                component={ProductDetail}
-                options={({route}) => ({
-                    title: route.params.name
-                })} />
-            </Stack.Navigator>
+            
         </NavigationContainer>
     )
 }
