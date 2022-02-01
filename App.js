4947db6c46c3ed8ce63c80224f99331807b6c427
Mariano Vergara <marianovf19@outlook.com>
2022-02-01 12:57:58 -0300

    integrando store a la app

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import AppNavigation from './src/navigation';
+import {Provider} from 'react-redux';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import store from './src/store/index';
 
 const App = () => {
-  return <AppNavigation />;
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
