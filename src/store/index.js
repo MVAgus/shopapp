9be436b18f92f7681cc3050d2ebe09323fd5ccec
Mariano Vergara <marianovf19@outlook.com>
2022-02-03 15:11:20 -0300

    order cart categories y bread añadidos al store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { combineReducers, createStore } from "redux";
 
 import BreadReducer from "./reducers/bread.reducer";
+import CartReducer from "./reducers/cart.reducer";
 import CategoryReducer from "./reducers/category.reducer";
+import OrderReducer from "./reducers/order.reducer";
 
 {/*este archivo se utiliza como root del directorio de store. 
     En este se configura como se combinan nuestros reducers que usaremos
@@ -9,7 +11,9 @@ importandolos y asociandolos en una estructura de objeto combine reducer*/}
 
 const RootReducer = combineReducers({
     categories: CategoryReducer,
-    breads: BreadReducer
+    breads: BreadReducer,
+    cart: CartReducer,
+    orders: OrderReducer
 })
 
 export default createStore(RootReducer);
