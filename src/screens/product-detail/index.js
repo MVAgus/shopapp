7229301544daeed796b5972699e9e3a36d82ccbf
Mariano Vergara <marianovf19@outlook.com>
2022-02-03 14:34:34 -0300

    se utiliza el useSelector para controlar el detalle del producto

--- src/screens/product-detail/index.js
+++ src/screens/product-detail/index.js
@@ -6,18 +6,19 @@ import {
 
 import React from 'react';
 import styles from './styles';
+import { useSelector } from 'react-redux';
 
 const ProductDetail = ({navigation, route}) => {
-   
-  const {product} = route.params
+
+  const bread = useSelector(state => state.breads.selected);
   
    return (
      <SafeAreaView style={styles.container}>
        <View style={styles.container}>
-         <Text>{product.name}</Text>
-         <Text>{product.description}</Text>
-         <Text>{product.weight}</Text>
-         <Text>$ {product.price}</Text>
+         <Text>{bread.name}</Text>
+         <Text>{bread.description}</Text>
+         <Text>{bread.weight}</Text>
+         <Text>$ {bread.price}</Text>
        </View>
      </SafeAreaView>
    );
